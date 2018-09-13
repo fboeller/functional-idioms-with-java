ff639f8900a75cea1e604429cace7c9657eabc88
Fabian Boeller <fabianchina@googlemail.com>
2018-09-13 21:12:54 +0200

    Modify build script, add presentation.pdf

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# Build LaTeX sources
+cd slides
+pdflatex presentation.tex
+pdflatex presentation.tex
+cp presentation.pdf ..
+cd ..
+
+# Build Java sources
 mkdir output &> /dev/null
 javac -sourcepath src/**/*.java -d output
